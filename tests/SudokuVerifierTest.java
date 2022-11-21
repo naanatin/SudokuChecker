3e68d6a3935a9fd62ebe1f47a9b4a243e832fb3d
naanatin <naanatin@gmail.com>
2022-11-21 15:59:05 +0200

    Update SudokuVerifierTest.java
    added more tests

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -1,5 +1,5 @@
+import static org.hamcrest.MatcherAssert.*;
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class SudokuVerifierTest {
@@ -10,6 +10,15 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 String c = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 String i = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+String shortInput = "41736982563215894795872431682543716979158643234691275828964357157329168416487529";
+String longInput = "4173698256321589479587243168254371697915864323469127582896435715732916841648752931";
+String hasChar = "4173698256321589479587243168254371a9791586432346912758289643571573291684164875293";
+String hasSpecialChar = "4173698256321589479587243168254371&9791586432346912758289643571573291684164875293";
+String hasZero = "417369825632158947958724316825437169790586432346912758289643571573291684164875293";
+// I made a valid Sudoku grid and altered it to get both row and column errors
+String rowtest = "123456789456789123789123456234567891567891234891234567345912678678345345912678912";
+String coltest = "123456789456789123789123456234567891567891234891234567435912678768345912192678345";
+
 SudokuVerifier v = new SudokuVerifier();
 
 	@Test
@@ -25,5 +34,56 @@ SudokuVerifier v = new SudokuVerifier();
 		
 	}
 	
+	@Test
+	public void testShortInput() {
+		int a = v.verify(shortInput);
+		assertEquals("short string", a, -1);
+	}
+	
+	@Test
+	public void testLongInput() {
+		int a = v.verify(longInput);
+		assertEquals("long string", a, -1);
+	}
+	
+	@Test
+	public void testInputHasNonnumericChar() throws NumberFormatException {
+		
+		try {
+			int a = v.verify(hasChar);
+			assertEquals("non-numeric string", a, 1);
+	    } catch (Exception e) {
+	        assertThat(e.getMessage(), e.getMessage().startsWith("java.lang.NumberFormatException"));
+	    }
+
+	}
+	
+	@Test
+	public void testInputHasSpecialChar() throws NumberFormatException {
+		try {
+			int a = v.verify(hasSpecialChar);
+			assertEquals("non-numeric special char string", a, 1);
+	    } catch (Exception e) {
+	        assertThat(e.getMessage(), e.getMessage().startsWith("java.lang.NumberFormatException"));
+	    }
+	}
+	
+	@Test
+	public void testInputHasZero() {
+		int a = v.verify(hasZero);
+		assertEquals("has zero string", a, -1);
+	}
+	
+	@Test
+	public void testRowCheckIsCorrect() {
+		int a = v.verify(rowtest);
+		assertEquals("one row is incorrect", a, -3);
+	}
+	
+	@Test
+	public void testColumnCheckIsCorrect() {
+		int a = v.verify(coltest);
+		assertEquals("one column is incorrect", a, -4);
+	}
 	
 }
